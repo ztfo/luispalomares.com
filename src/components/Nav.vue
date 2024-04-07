f9fcf5ccfc5281ba717076354c7d1e3ef2ed935a
Luis E. Palomares <lepalomares@me.com>
2024-04-07 18:21:57 -0500

    styling navigation

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
-nav.navbar(role="navigation", aria-label="main navigation")
-    .navbar-brand
+nav.navbar.p-5(role="navigation", aria-label="main navigation")
+    .navbar-brand.signal-brand
         a.navbar-item(href="/")
-            span signal
+            .square-line
+                font-awesome-icon(:icon="['fas', 'shekel-sign']", :rotation="90")
+            span.brand-name SIGNAL
         a.navbar-burger(role="button", aria-label="menu", aria-expanded="false", data-target="navbarBasicExample")
             span(aria-hidden="true")
             span(aria-hidden="true")
@@ -16,16 +18,16 @@ nav.navbar(role="navigation", aria-label="main navigation")
             a.navbar-item Insights
         .navbar-end
             .navbar-item
-                .buttons
-                a.button.search
-                    font-awesome-icon(icon="search")
-                a.button.notifications
-                    font-awesome-icon(icon="bell")
-                a.button.avatar
-                    .avatar-img z
-                    .avatar-name 
-                        span ztfo
-                        font-awesome-icon(icon="caret-down")
+                .account.buttons
+                    a.button.search
+                        font-awesome-icon(icon="search")
+                    a.button.notifications.is-active
+                        font-awesome-icon(icon="bell")
+                    a.button.avatar
+                        .avatar-name 
+                            span ztfo
+                        .avatar-img
+                            font-awesome-icon(icon="user-astronaut")
 
 </template>
 
@@ -53,5 +55,36 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar {
+    border-bottom: 1px solid rgba(255,255,255,0.1);
+}
+.signal.navbar-brand {
+    border: 2px solid #FFFFFF;
+}
+.square-line {
+      border: 2px solid #FFFFFF;
+      padding: 5px;
+      color: #FFF;
+      margin-right: .5rem;
+      line-height: 1;
+}
+.brand-name {
+    color: #FFF;
+    font-weight: bolder;
+    letter-spacing: 1px;
+}
+.account .button {
+    border: none;
+    outline: none;
+    background: transparent;
+}
+.avatar-img {
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 999px;
+    margin-left: .5rem;
+    background-color: var(--green);
+    color: var(--black);
+}
 </style>
