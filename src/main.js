44721fbf5bc0f058222b3ca484ec799d2c50ee53
Luis E. Palomares <lepalomares@me.com>
2024-04-06 22:05:07 -0500

    added bulma and sass loader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bulma/css/bulma.css'
+import '@/assets/styles/app.sass'
 
 createApp(App).use(router).mount('#app')
