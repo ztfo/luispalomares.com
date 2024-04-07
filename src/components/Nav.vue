e3f3c95ae451228e2420b04bee9ac9d890242023
Luis E. Palomares <lepalomares@me.com>
2024-04-06 22:21:20 -0500

    dashboard start

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,27 @@
 <template lang="pug">
-.nav
-    h1 Nav
+    nav.navbar(role="navigation", aria-label="main navigation")
+        .navbar-brand
+        a.navbar-item(href="/")
+            a.navbar-burger(role="button", aria-label="menu", aria-expanded="false", data-target="navbarBasicExample")
+            span(aria-hidden="true")
+            span(aria-hidden="true")
+            span(aria-hidden="true")
+            span(aria-hidden="true")
+        #navbar.navbar-menu
+        .navbar-start
+            a.navbar-item Overview
+            a.navbar-item Ledgers
+            a.navbar-item Payments
+            a.navbar-item Operations
+            a.navbar-item Insights
+            a.navbar-item Connectors
+        .navbar-end
+            a.navbar-item Payments
+            .navbar-item
+                .buttons
+                a.button.is-primary
+                    strong Sign up
+                a.button.is-light Log in
 </template>
 
 <script>
