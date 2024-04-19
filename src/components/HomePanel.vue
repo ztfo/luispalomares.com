472a9aeb14b8c29ed451a6d13d24af788991eceb
Luis E. Palomares <lepalomares@me.com>
2024-04-18 21:25:47 -0500

    minor copy edit for another test deploy wtf

--- src/components/HomePanel.vue
+++ src/components/HomePanel.vue
@@ -4,7 +4,7 @@
     .simple-divider
     p.is-size-5.mb-6
         span A 
-        a.link(href="https://www.linkedin.com/in/luis-palomares/", target="_blank") mission-driven leader
+        a.link(href="https://www.linkedin.com/in/luis-palomares/", target="_blank") mission-driven product leader
         |, who has championed tech initiatives that guard families against wire fraud in real estate, amplify transparency in home lending, and open credit avenues for immigrants, steering startups from 
         a.link(href="https://tucson.com/business/tucson-tech-ua-real-estate-services-startup-wins-major-investment/article_cf74558c-5f18-552b-81a0-cf60679257cf.html", target="_blank") seed 
         | to 
