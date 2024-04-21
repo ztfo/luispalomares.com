9b8d38d6bc0866cca133bdcaa1289d01de41f95a
Luis E. Palomares <lepalomares@me.com>
2024-04-21 17:50:03 -0500

    Updates

---
+++ js/399.c2bb3b2c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkLuis"]=self["webpackChunkLuis"]||[]).push([[399],{4399:function(n,t,e){e.r(t),e.d(t,{default:function(){return i}});var u=e(6768);const r={class:"company-component"};function s(n,t,e,s,c,o){const a=(0,u.g2)("Testing");return(0,u.uX)(),(0,u.CE)("div",r,[(0,u.bF)(a)])}var c={name:"CertifIDContent",data(){return{}},methods:{},mounted(){}},o=e(1241);const a=(0,o.A)(c,[["render",s]]);var i=a}}]);
+//# sourceMappingURL=399.c2bb3b2c.js.map
